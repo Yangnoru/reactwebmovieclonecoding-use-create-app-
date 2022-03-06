b2b993163e452f24a6ec274cad8b60b52b4593bd
YangSeonmo <tnwlr48@gmail.com>
2022-03-06 21:38:36 +0900

    divide and conquer [use css module]
    원하는 component에 원하는 css효과를 줄 수 있다.
    - 작성한 CSS는 CRA에의해 JS object로 변환된다.
    - class는 랜덤하게 생기기 때문에 다른 component에서 같은 class명을 쓰게되더라도 문제가 없다.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+
+import Button from './Button';
+import styles from './App.module.css';
+
+
 function App() {
   return (
     <div>
-      <h1>new start plz motivated!!!</h1>
+      <h1 className={styles.title}>new start plz motivated!!!</h1>
+      <Button text="continue"/>
     </div>
   );
 }
 
 export default App;
+
