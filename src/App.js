2741662794cfd69faac976c135d07cf419d92534
YangSeonmo <tnwlr48@gmail.com>
2022-03-07 23:34:16 +0900

    Cleanup Function
    component가 생성될때, 파괴될 때 특정 코드를 실행시킬 수 있다.
    하지만 자주 쓰이진 않는다.

--- src/App.js
+++ src/App.js
@@ -3,37 +3,30 @@ import Button from './Button';
 import styles from './App.module.css';
 
 
+function Hello(){
+  function destroyedFn(){
+    console.log("난 파괴됐다")
+  }
+
+  function createFn(){
+    console.log("난 만들어졌다");
+    return destroyedFn;
+  }
+  useEffect( createFn, [])
+  return(
+  <h1>Hello</h1>
+  )
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const onClick = () =>{
-    setValue((prev) => prev+1);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev)=> !prev);
   }
-  const onChange = (event) =>{
-    setKeyword(event.target.value);
-  }
-
-  console.log("i run all the time");
-
-  useEffect(()=>
-  {console.log("call the api")
-},[]);
-  useEffect( () => {
-    if(keyword !== "" && keyword.length > 5){
-      console.log ("Search For", keyword);
-    }
-},[keyword]);
-
   return (
     <div>
-      <input 
-      value={keyword} 
-      onChange={onChange} 
-      type="text" 
-      placeholder='Search here..!'/>
-      <h1 className={styles.title}>{counter}</h1>
-      <Button  text="continue" changeValue={onClick}/>
+    {showing ? <Hello/> : null}  
+    <button onClick={onClick}>{showing ? "hide" : "Show"}</button>
     </div>
   ); 
 }
