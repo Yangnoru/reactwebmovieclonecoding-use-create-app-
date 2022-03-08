5a511f6d09528413ef4bc38e28ca3986892c8645
YangSeonmo <tnwlr48@gmail.com>
2022-03-08 23:49:51 +0900

    새 배열 만들기
    Array state를 직접 건드리지 않고 바꾸는방법.

--- src/App.js
+++ src/App.js
@@ -3,30 +3,34 @@ import Button from './Button';
 import styles from './App.module.css';
 
 
-function Hello(){
-  function destroyedFn(){
-    console.log("난 파괴됐다")
-  }
-
-  function createFn(){
-    console.log("난 만들어졌다");
-    return destroyedFn;
-  }
-  useEffect( createFn, [])
-  return(
-  <h1>Hello</h1>
-  )
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing((prev)=> !prev);
+  const [toDo,setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => {
+    setToDo(event.target.value);
   }
+  const onSubmit = (event) =>{
+    event.preventDefault();
+    if (toDo ===""){
+      return;
+    }else{
+      setToDos(function(toDos){
+        return([...toDos,toDo])
+      })
+      setToDo("");
+    }
+  }
+  console.log(toDos);
+
+
   return (
     <div>
-    {showing ? <Hello/> : null}  
-    <button onClick={onClick}>{showing ? "hide" : "Show"}</button>
+      <h1>My To Dos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+       <input onChange ={onChange} value={toDo} type="text" placeholder=''/>
+       <button>Add To Do</button>
+      </form>
     </div>
   ); 
 }
